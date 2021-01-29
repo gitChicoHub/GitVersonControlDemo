d337899f116e995bb0cea091c76f718c7e8556c7
gitChicoHub <lozzi_az@gmail.com>
2021-01-29 16:56:08 -0500

    Initializing Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+load.initialize(async function () {
+});
+
+load.action("Action", async function () {
+
+    let transaction_01 = new load.Transaction("Start_01");
+    transaction_01.start();
+    
+    load.log(`Hello there! Let's debug`);
+    
+    var a = 5;
+    var b = 10;
+    var sum = 0;
+    sum = a + b;
+    load.log("The sum of a + b is: " + sum);
+
+    transaction_01.stop();
+    load.log("The transaction took " + transaction_01.duration + " milliseconds");
+
+ });
+
+load.finalize(async function () {
+});
